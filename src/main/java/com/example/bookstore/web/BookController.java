5c481ce463295a0232020c265de7637aefa04e22
16cats <97065659+16cats@users.noreply.github.com>
2023-10-12 19:41:25 +0300

    commit

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,5 +1,7 @@
 package com.example.bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
@@ -19,6 +22,12 @@ public class BookController {
 	
 	@Autowired
 	private CategoryRepository crepository;
+	
+	// Rest all books http://localhost:8080/books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }  
 	
 	//get mapping is better than request here(?)
 	@GetMapping("/booklist")
